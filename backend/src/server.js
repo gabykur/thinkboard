176d0b0f95360e19cbf0f8826ad492d380424f46
Gaby <gabrielekuraite@gmail.com>
2026-06-28 21:26:02 +0200

    create: mongodb, models Note

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,26 @@
 import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB();
 
 app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
-  console.log("Server started on PORT 5001");
+  console.log("Server started on PORT:", PORT);
 });
+
+// mongodb+srv://gabrielekuraite_db_user:1K2k15bTI9VOhk11@cluster0.guel7jd.mongodb.net/?appName=Cluster0
+
+
+
+
+
+
+
