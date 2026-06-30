54386b617513502cc2a5adb304aec1219a6b2c88
Gaby <gabrielekuraite@gmail.com>
2026-06-30 18:41:29 +0200

    frontend: use of hot toaster

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage'
 import CreatePage from './pages/CreatePage'
 import NoteDetailPage from './pages/NoteDetailPage'
+import toast from 'react-hot-toast'
 
 function App() {
   return (
     <div>
+      <button onClick={() => toast.success('Note created!')}>Click me</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
