dea96a55339c85284b73de7b1dada413256bbb14
Gaby <gabrielekuraite@gmail.com>
2026-07-01 14:58:13 +0200

    add: retrieve the notes in the UI

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Navbar from '../components/Navbar.jsx'
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
 import toast from 'react-hot-toast'
+import { Divide } from 'lucide-react';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false); // boolean
@@ -35,6 +36,20 @@ const HomePage = () => {
       <Navbar />
 
       {isRateLimited && < RateLimitedUI />}
+
+      <div className='max-w-7xl mx-auto p-4 mt-6'>
+        {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {notes.map((note) => (
+              <div>
+                {note.title} | {note.content}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
