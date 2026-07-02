697694e6b4d17b05389d5aa1500e5f866ac2aad4
Gaby <gabrielekuraite@gmail.com>
2026-07-02 17:46:46 +0200

    feat: handle the delete functionality

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,26 @@
 import { PenSquareIcon, Trash2Icon } from 'lucide-react';
 import { Link } from 'react-router'
 import { formatDate } from '../lib/utils';
+import api from '../lib/axios';
+import toast from 'react-hot-toast';
 
-const NoteCard = ({note}) => {
+const NoteCard = ({note, setNotes}) => {
+
+  const handleDelete = async (e,id) => {
+    e.preventDefault(); //get rid of the navigation behaviour
+
+    if(!window.confirm("Are you sure to delete this note")) return;
+
+    try {
+      await api.delete(`/notes/${id}`)
+      setNotes((prev) => prev.filter(note => note._id !== id)) // get rid off the deleted one
+      toast.success("Note deleted successfully!")
+    } catch (error) {
+      console.log("Error in handleDelete", error)
+      toast.error("Failed to delete note.")
+    }
+  }
+
   return <Link to={`/note/${note._id}`} 
   className='card bg-base-100 hover:shadow-lg transition-all duration-200
     border-t-4 border-solid border-[#00FF9D]'
@@ -17,7 +35,7 @@ const NoteCard = ({note}) => {
         <div className='flex items-center gap-1'>
           <PenSquareIcon className='size-4'/>
           <button className='btn btn-ghost btn-xs text-error'>
-            <Trash2Icon className='size-4'/>
+            <Trash2Icon className='size-4' onClick={(e) => handleDelete(e,note._id)}/>
           </button>
         </div>
       </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -43,7 +43,7 @@ const HomePage = () => {
         {notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {notes.map((note) => (
-              <NoteCard  key={note._id} note={note}/> // note={note} is a prop
+              <NoteCard  key={note._id} note={note} setNotes={setNotes}/> // note={note} is a prop
             ))}
           </div>
         )}
