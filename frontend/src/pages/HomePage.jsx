bc9729bb63f59d840683927a0685d36582873767
Gaby <gabrielekuraite@gmail.com>
2026-07-03 16:24:59 +0200

    create: NoteNotFound component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar.jsx'
 import RateLimitedUI from '../components/RateLimitedUI.jsx';
 import toast from 'react-hot-toast'
 import NoteCard from '../components/NoteCard.jsx';
+import NotesNotFound from '../components/NotesNotFound.jsx';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false); // boolean
@@ -39,6 +40,8 @@ const HomePage = () => {
 
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
+
+        {notes.length < 0 && !isRateLimited && <NotesNotFound/>}
 
         {notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
